77d12b6cde42c58c5567074374b246e0fea8d8c8
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-06-06 18:21:04 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from "vue";
+import "./style.scss";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
