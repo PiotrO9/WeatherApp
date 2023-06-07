d0b3a9493798b7001d54a4d42a6ccd911f8b0c39
PiotrO9 <piotr.ostrowski02@wp.pl>
2023-06-07 11:54:04 +0200

    Add bootstrap elements for login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,112 @@
 <template>
-    <h1>Login view</h1>
+    <transition>
+        <main class="form-signin w-100 m-auto">
+            <form class="form-signin">
+                <div class="text-center mb-4">
+                    <h1 class="h1 mb-3 text-md font-weight-normal">Logowanie</h1>
+                </div>
+                <div class="form-label-group">
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                    <label for="inputEmail">Email</label>
+                </div>
+                <div class="form-label-group">
+                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                    <label for="inputPassword">Hasło</label>
+                </div>
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Zapamiętaj mnie
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Zaloguj się</button>
+            </form>
+        </main>
+    </transition>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-}
+export default defineComponent({
+
+})
 </script>
 
-<style></style>
+<style scoped lang="scss">
+@import "../GeneralStyles/variables";
+
+main {
+    @include SquareSize(100%);
+    @include FlexCenter;
+    background-image: url("../../public/sky-background.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    form {
+        width: 15%;
+        min-width: 200px;
+        font-family: $PTSans;
+
+        .form-label-group {
+            position: relative;
+            margin-bottom: 1rem;
+
+            input {
+                height: auto;
+                padding: 10px;
+                font-size: 16px;
+
+                &::-webkit-input-placeholder {
+                    color: transparent;
+                }
+
+                &:-ms-input-placeholder {
+                    color: transparent;
+                }
+
+                &::-moz-placeholder {
+                    color: transparent;
+                }
+
+                &:not(:placeholder-shown) {
+                    padding-top: 1.25rem;
+                    padding-bottom: 0.25rem;
+
+                    ~label {
+                        padding-top: 0.25rem;
+                        padding-bottom: 0.25rem;
+                        font-size: 12px;
+                        color: #777;
+                    }
+                }
+            }
+
+            >label {
+                position: absolute;
+                top: 0;
+                left: 0;
+                display: block;
+                width: 100%;
+                padding: 10px;
+                margin-bottom: 0;
+                line-height: 1.5;
+                color: #495057;
+                background-color: transparent;
+                pointer-events: none;
+                cursor: text;
+                border: 1px solid transparent;
+                border-radius: .25rem;
+                transition: all .1s ease-in-out;
+            }
+
+            >input,
+            >label {
+                padding: 10px;
+                height: auto;
+                border-radius: 4px;
+            }
+        }
+    }
+}
+</style>
